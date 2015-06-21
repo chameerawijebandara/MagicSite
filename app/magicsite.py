c84257e09439d66cebfb61ef36bf575572bb6c8a
chameerawijebandara <chameerawijebandara@gmail.com>
2015-06-21 22:58:14 +0530

    Add treecopy methord

--- app/magicsite.py
+++ app/magicsite.py
@@ -1,7 +1,48 @@
 import os
+from shutil import copytree, ignore_patterns
 
 class IOHandler():
-	"""This class handle all the IO functions"""	
+	"""This class handle all the IO functions"""
+	
+	@staticmethod	
+	def copytree(src, dst, symlinks=False, ignore=None):
+		names = os.listdir(src)
+	 	if ignore is not None:
+			ignored_names = ignore(src, names)
+		else:
+			ignored_names = set()
+	
+		os.makedirs(dst)
+		errors = []
+		for name in names:
+			if name in ignored_names:
+				continue
+			srcname = os.path.join(src, name)
+			dstname = os.path.join(dst, name)
+			try:
+				if symlinks and os.path.islink(srcname):
+					linkto = os.readlink(srcname)
+					os.symlink(linkto, dstname)
+				elif os.path.isdir(srcname):
+					copytree(srcname, dstname, symlinks, ignore)
+				else:
+					copy2(srcname, dstname)
+				# XXX What about devices, sockets etc.?
+			except (IOError, os.error) as why:
+				errors.append((srcname, dstname, str(why)))
+			# catch the Error from the recursive copytree so that we can
+			# continue with other files
+			except Error as err:
+				errors.extend(err.args[0])
+		try:
+			copystat(src, dst)
+		except WindowsError:
+			# can't copy file access times on Windows
+			pass
+		except OSError as why:
+			errors.extend((src, dst, str(why)))
+		if errors:
+			raise Error(errors)
 	
 	@staticmethod	
 	def __createFolder(folder):
@@ -20,7 +61,7 @@ class IOHandler():
 		for folder in folders:
 			IOHandler.__createFolder(folder)
 		for filename in files:		
-			IOHandler.__createFile(filename)
+			IOHandler.__createFile(filename)		
 	
 class MagicSite():
 	"""This is main class of the Magic"""
@@ -28,11 +69,16 @@ class MagicSite():
 	def __init__(self):
 		"""intitlaization of the magic """
 		self.folders = {"build/public", "build/src"}
-		self.files = {"build/index.md"}	
+		self.files = {"build/index.md"}
+		self.base = "src"
+		self.output = "build/src"	
 	
 	def boostreap(self):
 		"""This method is call when the new site is bootstrapping"""
+		#self.output = ofilepath
 		IOHandler.createFolderStructure(self.folders, self.files)
+		IOHandler.copytree(self.base, self.output)
+		
 
 
 
--- app/main.py
+++ app/main.py
@@ -1,10 +1,33 @@
 import magicsite
+import sys, getopt
 
-def main():
-     m = magicsite.MagicSite();
-     m.boostreap();
-
+def main(argv):
+   #inputfile = ''
+   #outputfile = ''
+   site = magicsite.MagicSite()
+   site.boostreap()    
+   """
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:b:",["ifile=","ofile=","boostreap="])
+   except getopt.GetoptError:
+      print 'main.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+         
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()   
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+      elif opt in ("-b", "--boostreap"):  
+            site.boostreap()    
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
+   
+   """
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
 	
 	
